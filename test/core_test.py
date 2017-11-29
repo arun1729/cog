da2e4f6f29d5d2ff9e052fac9e65faf7ae9a1b09
arun.mahendra@gmail.com <arun.mahendra@oracle.com>
2017-11-29 17:24:41 -0600

    test package re org

--- test/core_test.py
+++ test/core_test.py
@@ -1,8 +1,9 @@
-from core import Index
-from core import Store
-from core import Table
-from database import Cog
-import config
+from context import cog
+from cog.core import Index
+from cog.core import Store
+from cog.core import Table
+from cog import database
+from cog import config
 import logging
 import os
 
@@ -10,27 +11,26 @@ from logging.config import dictConfig
 
 if (not os.path.exists("/tmp/cog-test/test_table/")):
     os.mkdir("/tmp/cog-test/test_table/")
-    
+
 dictConfig(config.logging_config)
 logger = logging.getLogger()
- 
+
 data = ("new super data","super new old stuff")
- 
+
 table = Table("testdb","test_table","test_xcvzdfsadx")
- 
+
 store = Store(table,config,logger)
 index = Index(table,config,logger)
- 
- 
+
+
 position=store.save(data)
 print "stored"
-    
+
 index.put(data[0],position,store)
 print "indexed"
- 
+
 
 data=index.get(data[0], store)
 print "retrieved data: "+str(data)
 
 index.delete(data[0],store)
- 
