633d1cb899b2f3c220fcdd6888f18d26313c193a
arun.mahendra@gmail.com <arun.mahendra@oracle.com>
2017-11-30 11:14:52 -0600

    unit test updated

--- test/core_test.py
+++ test/core_test.py
@@ -6,31 +6,65 @@ from cog import database
 from cog import config
 import logging
 import os
-
 from logging.config import dictConfig
 
-if (not os.path.exists("/tmp/cog-test/test_table/")):
-    os.mkdir("/tmp/cog-test/test_table/")
+import unittest
 
-dictConfig(config.logging_config)
-logger = logging.getLogger()
+class TestStringMethods(unittest.TestCase):
 
-data = ("new super data","super new old stuff")
+    def test_put_get(self):
+        if (not os.path.exists("/tmp/cog-test/test_table/")):
+            os.mkdir("/tmp/cog-test/test_table/")
 
-table = Table("testdb","test_table","test_xcvzdfsadx")
+        dictConfig(config.logging_config)
+        logger = logging.getLogger()
 
-store = Store(table,config,logger)
-index = Index(table,config,logger)
+        expected_data = ("new super data","super new old stuff")
 
+        table = Table("testdb","test_table","test_xcvzdfsadx")
 
-position=store.save(data)
-print "stored"
-
-index.put(data[0],position,store)
-print "indexed"
+        store = Store(table,config,logger)
+        index = Index(table,config,logger)
 
 
-data=index.get(data[0], store)
-print "retrieved data: "+str(data)
+        position=store.save(expected_data)
+        print "stored"
 
-index.delete(data[0],store)
+        index.put(expected_data[0],position,store)
+        print "indexed"
+
+
+        returned_data=index.get(expected_data[0], store)
+        print "retrieved data: "+str(returned_data)
+        self.assertEqual(expected_data, returned_data[1])
+
+    def test_delete(self):
+        if (not os.path.exists("/tmp/cog-test/test_table/")):
+            os.mkdir("/tmp/cog-test/test_table/")
+
+        dictConfig(config.logging_config)
+        logger = logging.getLogger()
+
+        expected_data = ("new super data","super new old stuff")
+
+        table = Table("testdb","test_table","test_xcvzdfsadx")
+
+        store = Store(table,config,logger)
+        index = Index(table,config,logger)
+
+
+        position=store.save(expected_data)
+        print "stored"
+
+        index.put(expected_data[0],position,store)
+        print "indexed"
+
+        index.delete(expected_data[0],store)
+
+        returned_data=index.get(expected_data[0], store)
+        print "retrieved data: "+str(returned_data)
+        self.assertEqual(None, returned_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
