2afb34f08434863327fab5b8bd4b4b04c72a0c8a
Arun Mahendra <arun.mahendra@gmail.com>
2017-12-22 16:47:33 -0600

    sql select parser

---
+++ cog/queryengine.py
@@ -0,0 +1,51 @@
+import re
+
+# SQL Grammar reference: https://forcedotcom.github.io/phoenix/
+
+COMMAND_LIST = ["SELECT", "FROM", "WHERE"]
+
+
+class Planner:
+
+    def __init__(self, index, store):
+        self.index = index
+        self.store = store
+
+    def execute(self, statement):
+        print self.get_query_list(statement)
+
+    def get_query_list(self, statement):
+        query_list = statement.split(";")
+        if len(query_list) == 1 and query_list[0] == '':
+            return None
+
+        if len(query_list) > 1 and query_list[-1] == '':
+            query_list = query_list[0:-1]
+
+        return query_list
+
+    def get_select_command(self, query):
+
+        select_tokens = re.split(COMMAND_LIST[0], query, flags=re.IGNORECASE)
+        assert select_tokens[0] is '', "Syntax error: a query must start with Select."
+        assert len(select_tokens) == 2, "Syntax error: invalid select statement."
+
+        from_tokens = re.split(COMMAND_LIST[1], select_tokens[1], flags=re.IGNORECASE)
+        assert len(from_tokens) == 2, "Syntax error: invalid select statement at FROM command."
+
+        columns_str = from_tokens[0]
+        columns = []
+        for c in columns_str.split(","):
+            columns.append(c.strip())
+
+        where_tokens = re.split(COMMAND_LIST[2], from_tokens[1], flags=re.IGNORECASE)
+
+        where_conditions = []
+        if len(where_tokens) > 1:
+            where_conditions_str = where_tokens[1]
+            for w in re.split('AND | , | OR ', where_conditions_str, flags=re.IGNORECASE):
+                where_conditions.append(w.strip())
+        else:
+            where_conditions = None
+
+        return columns, where_conditions
--- test/queryEngine_test.py
+++ test/queryEngine_test.py
@@ -1,4 +1,4 @@
-from cog.QueryEngine import Planner
+from cog.queryengine import Planner
 import unittest
 
 
@@ -11,6 +11,13 @@ class TestQueryEngine(unittest.TestCase):
         query_list = planner.get_query_list('select username from test_table; select username, email from test_table')
         self.assertEqual(query_list[0], 'select username from test_table')
         self.assertEqual(query_list[1].strip(), 'select username, email from test_table')
+
+    def test_select_parser(self):
+        planner = Planner(None, None)
+        select_cmd = planner.get_select_command('select username from test_table where firstname = "Hari" and lastname = "Seldon"')
+        self.assertEqual(select_cmd[0], ['username'])
+        self.assertEqual(select_cmd[1], ['firstname = "Hari"', 'lastname = "Seldon"'])
+
 
 if __name__ == '__main__':
     unittest.main()
