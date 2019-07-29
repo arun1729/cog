2d0154a92a62f103b87a866abcf2b26a9452aee9
arun1729 <arun.mahendra@gmail.com>
2019-07-29 09:31:36 -0400

    setup.py update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='cogdb',
-      version='0.0.4',
-      description='Persistant Key Value Store',
+      version='0.0.5',
+      description='Persistent Key Value Store',
       url='http://github.com/arun1729/cog',
       author='Arun Mahendra',
-      author_email='',
+      author_email='arunm3.141@gmail.com,
       license='MIT',
       packages=['cog'],
       zip_safe=False)
