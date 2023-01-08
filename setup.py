d080c998e2dc012d1c60f4ee39ef9dc7650b96d9
arun1729 <arun.mahendra@gmail.com>
2023-01-07 20:58:49 -0500

    upgrade xxhash 3.2.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(name='cogdb',
-      version='3.0.2',
+      version='3.0.3',
       description='Persistent Embedded Graph Database',
       url='http://github.com/arun1729/cog',
       author='Arun Mahendra',
       author_email='arunm3.141@gmail.com',
       license='MIT',
       packages=['cog'],
-      install_requires=['xxhash==2.0.0'],
+      install_requires=['xxhash==3.2.0'],
       zip_safe=False)
