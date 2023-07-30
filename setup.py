687b9530646939b776670e6283354032d28be304
arun1729 <arun.mahendra@gmail.com>
2023-07-29 21:43:44 -0400

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='cogdb',
-      version='3.0.6',
+      version='3.0.7',
       description='Persistent Embedded Graph Database',
       url='http://github.com/arun1729/cog',
       author='Arun Mahendra',
