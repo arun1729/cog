b6a640b7f296e173a5dfe7b3f16629dba77554f1
arun1729 <arun.mahendra@gmail.com>
2026-01-07 16:49:31 -0500

    Remove pyproject.toml, fix build

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
