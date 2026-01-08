1facd83ae278ed66798c7bf385df2e7f142aa735
arun1729 <arun.mahendra@gmail.com>
2026-01-08 17:05:45 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cogdb',
-    version='3.4.2',
+    version='3.4.3',
     description='Persistent Embedded Graph Database',
     url='http://github.com/arun1729/cog',
     author='Arun Mahendra',
